1e45e8a3b1cdc757c06d215b4a93bbbcb21eea95
desto4q <desto4q@gmail.com>
2024-03-03 13:43:08 +1300

    new

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,42 @@
-import React from "react";
-
+import {
+	Burger,
+	Center,
+	Drawer,
+	Flex,
+	Image,
+	Portal,
+	Stack,
+	Text,
+	Title
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 function Nav() {
+	let [opened, { open, close }] = useDisclosure();
 	return (
-		<nav className="h-100 bg-red-500 w-full">
-			<div className="mx-auto container">sis</div>
-		</nav>
+		<>
+			<Portal>
+				<Drawer onClose={close} opened={opened}></Drawer>
+			</Portal>
+			<nav className="h-20 py-2 bg-red-500 w-full flex align-middle justify-center px-4 ">
+				<Flex
+					justify={"center"}
+					w={{ base: "100%", lg: "90%" }}
+					className="bg-blue-200 p-1 rounded-md"
+				>
+					<Flex gap={"lg"} mr={"auto"}>
+						<Image src={"ava.png"} h={"100%"} className="rounded-full"></Image>
+						<Stack gap={0} bg={"green"}>
+							<Title order={3}>Hey there</Title>
+							<Text>Web developer</Text>
+						</Stack>
+					</Flex>
+					<Flex></Flex>
+					<Center className=" md:hidden">
+						<Burger opened={opened} onClick={open}></Burger>
+					</Center>
+				</Flex>
+			</nav>
+		</>
 	);
 }
 
